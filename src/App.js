5e0aa1652b41562ce684f2df61cf087c37425c80
n00bNn00b <reldewan9@gmail.com>
2022-09-30 19:06:00 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Create from "./components/Create/Create";
 import Event from "./components/Event/Event";
 import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/event" element={<Event />} />
