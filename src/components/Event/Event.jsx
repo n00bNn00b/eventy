70549597204a696e48f31c8bc21a31904af84740
n00bNn00b <reldewan9@gmail.com>
2022-09-30 18:36:37 +0600

    event page implemented

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import { FaCalendar, FaSearchLocation } from "react-icons/fa";
 
 const Event = () => {
   return (
-    <div className="flex lg:flex-row md:flex-col-reverse sm:flex-col-reverse my-20 md:justify-center md:items-center ">
-      <div className="md:text-start my-20">
+    <div className="flex lg:flex-row md:flex-col-reverse sm:flex-col-reverse my-20 md:justify-center md:items-start ">
+      <div className="md:mx-auto sm:mx-auto md:ml-48 sm:ml-40 my-20">
         <h1 className="text-4xl font-bold">Birthday Bash</h1>
         <p className="text-[#828282]">
           Hosted by <span className="font-bold"> Elysia</span>
         </p>
-        <div>
-          icon. 18 August 6:00PM
-          <p>to 19 August 1:00PM UTC+10</p>{" "}
+        {/*  event date*/}
+        <div className="flex mt-10">
+          <div className="bg-white p-2 border h-1/2 rounded">
+            <FaCalendar className="text-secondary text-2xl " />
+          </div>
+
+          <div className="ml-5">
+            <h2 className="text-[#240D57] font-bold text-2xl">
+              18 August 6:00PM
+            </h2>
+            <p>
+              to{" "}
+              <span className="text-[#4F4F4F] text-2xl font-bold">
+                19 August 1:00PM
+              </span>{" "}
+              <span className="text-2xl">UTC+10</span>
+            </p>{" "}
+          </div>
         </div>
-        <div>
-          incon Street name
-          <p>Suburb, State, Postcode</p>
+        <div className="flex mt-10">
+          <div className="bg-white h-1/2 p-2 border rounded">
+            <FaSearchLocation className="text-secondary text-2xl " />
+          </div>
+
+          <div className="ml-5">
+            <h2 className="text-[#240D57] font-bold text-2xl">Street name</h2>
+            <p className="text-[#4F4F4F] text-2xl">Suburb, State, Postcode</p>
+          </div>
         </div>
-        <div></div>
       </div>
-      <div className="">
+      <div className="my-20 md:mx-auto sm:mx-auto md:ml-48 sm:ml-40">
         <img
-          className="w-96"
+          className="w-96 border-r-4"
           src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/17d6299f-f287-469c-a403-b8ab9d75aa62/Birthday_cake.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220930%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220930T085604Z&X-Amz-Expires=86400&X-Amz-Signature=dbd3d7abf99f595d001776b8b403647bdfb2cd329d87364b5904d4787412ecd1&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Birthday%2520cake.png%22&x-id=GetObject"
           alt="birthday"
         />
